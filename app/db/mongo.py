305bef990a52128854a0b234332496bd8d319d1a
Nitin Kumar <nitin.kumar14@nagarro.com>
2026-08-20 15:12:24 +0530

    fix

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -17,30 +17,40 @@ def get_client() -> MongoClient:
 
 def get_db():
     """
-    Return the default MongoDB database from the URI.
+    Return the MongoDB database defined in the URI.
 
     Example:
-        mongodb+srv://user:password@cluster.mongodb.net/smart_learning_lab
-
-    get_default_database() will return:
-        smart_learning_lab
+    mongodb+srv://user:password@cluster.mongodb.net/smart_learning_lab
     """
     client = get_client()
-
-    db = client.get_default_database()
-
-    return db
+    return client.get_default_database()
 
 
 def get_database():
-    """
-    Backward-compatible alias.
-    """
+    """Backward-compatible alias."""
     return get_db()
+
+
+def ping() -> bool:
+    """
+    Check whether MongoDB is reachable.
+    """
+    try:
+        get_client().admin.command("ping")
+        return True
+    except Exception:
+        return False
+
+
+def close():
+    """
+    Close MongoDB connection.
+    """
+    get_client().close()
 
 
 def close_mongo_connection():
     """
-    Close the MongoDB client.
+    Backward-compatible alias.
     """
-    get_client().close()
+    close()
